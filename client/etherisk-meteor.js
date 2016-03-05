63d8797f8b1577ab67732b3806efb20b92adec2d
Jasmine Kent <jk@codeartemis.com>
2016-03-05 17:59:06 +0100

    wip

--- client/etherisk-meteor.js
+++ client/etherisk-meteor.js
@@ -1,9 +1,21 @@
-Meteor.startup(function() {
-  EthAccounts.init();
-});
+Session.set('contractAddress', '0xB4C4CAfe4f5514825602868cD62dB069bb40Bc5f');
+
+EthAccounts.init();
+
+function account() {
+  // Just return the first account we find.
+  return EthAccounts.findOne();
+}
 
 Template.status.helpers({
-  account: function() {
-    return EthAccounts.findOne()._id;
+  contractAddress: function() {
+    return Session.get('contractAddress');
+  },
+  acct: function() {
+    var acct = account();
+    console.log(acct);
+    var val = acct && acct.address;
+    console.log(val);
+    return val;
   }
 });
